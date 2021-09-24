22f59ca5149a0849f33634a37e4b4bfb7e8e7040
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-24 17:41:18 +0200

    Update Address.php

--- src/Address.php
+++ src/Address.php
@@ -2,19 +2,34 @@
 
 namespace Bermuda\Mail;
 
-use Bermuda\String\Stringable;
+use Stringable;
 
 /**
- * Class Address
- * @package Bermuda\Mail
+ * @property-read string $email
+ * @property-read string $name
  */
 final class Address implements Stringable
 {
-    private string $value, $name;
+    private string $email, $name;
 
-    public function __construct(string $value, string $name = '')
+    public function __construct(string $email, string $name = '')
     {
-        $this->value = $value; $this->name = $name;
+        $this->email = $email;
+        $this->name = $name;
+
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            throw new \InvalidArgumentException('Invalid email: '. $email);
+        }
+    }
+
+    public function __get(string $name)
+    {
+        switch ($name){
+            case 'name': return $this->name;
+            case 'email': return $this->email;
+        }
+
+        return null;
     }
 
     /**
@@ -22,7 +37,7 @@ final class Address implements Stringable
      */
     public function __toString(): string
     {
-        return $this->value;
+        return $this->email;
     }
 
     /**
